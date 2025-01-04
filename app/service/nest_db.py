800e6fb73269e9dbdf5e738c1b1ac668f26975b7
jarin.nemec <jarin.nemec@gmail.com>
2025-01-04 10:48:59 +0100

    add spike for data analysis

--- app/service/nest_db.py
+++ app/service/nest_db.py
@@ -1,10 +1,13 @@
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta, date
 
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
+from app import configuration
 from app.service.nest_watcher_schema.information_schema import InformationSchema
 from app.service.nest_watcher_schema.nest_record import NestRecord, map_nestInfo_data_to_dto
 from app.service.nest_watcher_schema.nest_watcher_schema_creator import CoopMasterDBCreator
@@ -61,7 +64,52 @@ class NestSellDB:
     def close(self):
         self.session.close()
 
+    def get_todays_records_per_nest(self, nest):
+
+        current_time = datetime.now()
+        days_ms = datetime.combine(date.today(), datetime.min.time())
+        days_ms = datetime(2025, 1, 2, 0, 0, 0)
+        condition = (NestRecord.timestamp >= days_ms)
+
+        try:
+            count = self.session.query(NestRecord).filter(condition).filter(NestRecord.nest_id == nest).all()
+            return count
+        except Exception as e:
+            logging.error(f"Could not get records count: {e}")
+            return "Could not get records count"
+
 
 if __name__ == '__main__':
-    CoopMasterDBCreator(host="localhost", port=5432, database="coopmaster", user="coop_admin",
-                        password="your_secure_password")
+    matplotlib.use('TkAgg', force=True)
+
+    #    CoopMasterDBCreator(host="localhost", port=5432, database="coopmaster", user="coop_admin",
+    #                        password="your_secure_password")
+
+    db = NestSellDB(host="localhost", port=5432, database="coopmaster", user="coop_admin",
+                    password="your_secure_password")
+    a = db.get_nest_record_count()
+
+    nests = configuration.construct_nests_from_env()
+
+
+    # todo: fixnout datetime.now pri tvorbe recordu protoze je to o hodinu pozde
+    # todo:
+
+
+
+    for nest in nests:
+        count = db.get_todays_records_per_nest(nest["name"])
+        weights = []
+        for item in count:
+            weights.append(item.weight)
+
+        plt.plot(weights)
+
+        # Label axes and provide a title
+        plt.xlabel('Index')
+        plt.ylabel('Hmotnost')
+        plt.title(nest)
+
+        # Display the plot
+        plt.show()
+    aa = 0
